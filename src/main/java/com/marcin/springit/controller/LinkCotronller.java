a561ad5e7458abb1eb4170d87dad3f997780903b
Marcin Zienkiewicz <mzienkiewicz@objectivity.co.uk>
2019-01-07 15:59:48 +0100

    add link controler

---
+++ src/main/java/com/marcin/springit/controller/LinkCotronller.java
@@ -0,0 +1,48 @@
+package com.marcin.springit.controller;
+
+import com.marcin.springit.domain.Link;
+import com.marcin.springit.repository.LinkRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/links")
+public class LinkCotronller {
+
+    private LinkRepository linkRepository;
+
+    public LinkCotronller(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    // list
+    @GetMapping("/")
+    public List<Link> list() {
+        return linkRepository.findAll();
+    }
+
+    // CRUD
+
+    @PostMapping("/create")
+    public Link create(@ModelAttribute Link link) {
+        return linkRepository.save(link);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Link> read(@PathVariable Long id) {
+        return linkRepository.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Link update(@PathVariable Long id, @ModelAttribute Link link) {
+        //get the id
+        return linkRepository.save(link);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        linkRepository.deleteById(id);
+    }
+}
